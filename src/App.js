993a371dee1d7e8e3b08d19d8cf9271fccd64002
Jose Antonio Olivari <antonio.olivari@gmail.com>
2021-05-01 11:57:19 +0200

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import CaughtPokemon from './components/CaughtPokemon'
 
+
+
+const Logo = ()=>{
+  let appName = "Pokedex"
+  return(
+    <header>
+      <p>
+        {appName}
+      </p>
+      <h1>Welcome to the Pokedex</h1>  
+      <img 
+        alt = 'pokedex' 
+        src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png'
+      ></img>
+    </header>
+  );
+  };
+  
+const BestPokemon = ()=>{
+  let abilities = ['Anticipation', 'Adaptability', 'Run-Away']
+  return(
+  <div>
+   <p>
+    Mi pokemon favorito es Squirtle
+  </p>
+    <ul>{abilities.map((abilities, index) => {
+      return(<li key = {index}>{abilities} {index}</li>)
+    })}
+    </ul>
+  </div>
+  )
+}
+  
+  
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+    <>
+    <Logo/>
+    <CaughtPokemon/>
+    <BestPokemon/>
+    </>
   );
 }
 
---
+++ src/components/CaughtPokemon.js
@@ -0,0 +1,14 @@
+
+
+
+function CaughtPokemon(){
+    let date =  new Date().toLocaleDateString()
+      return(
+        <p>
+          Caught 0 Pokemon on {date}
+        </p>
+        
+      )
+    };
+
+export default CaughtPokemon
